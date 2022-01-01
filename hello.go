90a06d84ec1f013a68e38ecfa01db7fa1eb20706
wwaaoonn <69627172+wwaaoonn@users.noreply.github.com>
2022-01-01 10:37:28 +0900

    scroll

--- hello.go
+++ hello.go
@@ -3,9 +3,9 @@ package main
 import (
 	"fmt"
 
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
 )
 
@@ -16,22 +16,22 @@ func main() {
 	w := a.NewWindow("Hello")
 
 	w.SetContent(
-		container.New(
-			layout.NewGridLayoutWithRows(3),
-			widget.NewButton("One", nil),
-			widget.NewButton("Two", nil),
-			widget.NewButton("Three", nil),
-			widget.NewButton("Four", nil),
-			layout.NewSpacer(),
-			widget.NewButton("Five", nil),
-			widget.NewButton("Six", nil),
-			layout.NewSpacer(),
-			widget.NewButton("Seven", nil),
-			widget.NewButton("Eight", nil),
-			widget.NewButton("Nine", nil),
-			widget.NewButton("Ten", nil),
+		container.NewScroll(
+			container.NewVBox(
+				widget.NewButton("One", func() { fmt.Println("One") }),
+				widget.NewButton("Two", func() { fmt.Println("Two") }),
+				widget.NewButton("Three", func() { fmt.Println("Three") }),
+				widget.NewButton("Four", func() { fmt.Println("Four") }),
+				widget.NewButton("Five", func() { fmt.Println("Five") }),
+				widget.NewButton("Six", func() { fmt.Println("Six") }),
+				widget.NewButton("Seven", func() { fmt.Println("Seven") }),
+				widget.NewButton("Eight", func() { fmt.Println("Eight") }),
+				widget.NewButton("Nine", func() { fmt.Println("Nine") }),
+				widget.NewButton("Ten", func() { fmt.Println("Ten") }),
+			),
 		),
 	)
 
+	w.Resize(fyne.NewSize(200, 200))
 	w.ShowAndRun()
 }
