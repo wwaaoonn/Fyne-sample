8984474ca8a2f4a6add3d7373618681768b9b44a
wwaaoonn <69627172+wwaaoonn@users.noreply.github.com>
2022-01-01 15:45:24 +0900

    my entry

--- hello.go
+++ hello.go
@@ -6,10 +6,29 @@ import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/dialog"
-	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
 )
+
+type MyEntry struct {
+	widget.Entry
+	entered func(e *MyEntry)
+}
+
+func NewMyEntry(f func(e *MyEntry)) *MyEntry {
+	e := &MyEntry{}
+	e.ExtendBaseWidget(e)
+	e.entered = f
+	return e
+}
+
+func (e *MyEntry) KeyDown(key *fyne.KeyEvent) {
+	switch key.Name {
+	case fyne.KeyReturn, fyne.KeyEnter:
+		e.entered(e)
+	default:
+		e.Entry.KeyDown(key)
+	}
+}
 
 func main() {
 	fmt.Println("start!")
@@ -17,29 +36,16 @@ func main() {
 	a := app.New()
 	w := a.NewWindow("Hello")
 	l := widget.NewLabel("Hello Fyne!")
-	ne := widget.NewEntry()
-	pe := widget.NewPasswordEntry()
-	fr := widget.NewForm(
-		widget.NewFormItem("Name", ne),
-		widget.NewFormItem("Pass", pe),
-	)
-	b := widget.NewButton("Click", func() {
-		dialog.ShowCustomConfirm("Enter message.", "OK", "Cancel", fr, func(b bool) {
-			if b {
-				l.SetText("Name: '" + ne.Text + "' Pass: '" + pe.Text + "'")
-			} else {
-				l.SetText("no message...")
-			}
-		}, w)
+	e := NewMyEntry(func(e *MyEntry) {
+		s := e.Text
+		e.SetText("")
+		l.SetText("you type '" + s + "'.")
 	})
 
 	w.SetContent(
-		container.New(
-			layout.NewBorderLayout(
-				nil, b, nil, nil,
-			),
+		container.NewVBox(
 			l,
-			b,
+			e,
 		),
 	)
 
